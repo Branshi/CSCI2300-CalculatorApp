7361b12fa46fea578e2cac024394c2213d931797
Subomiadekunle <subomiade25@gmail.com>
2025-04-22 18:05:27 -0500

    changed font (to lok more like desmos)

--- app/src/main/java/view/InputButton.java
+++ app/src/main/java/view/InputButton.java
@@ -1,16 +1,50 @@
+// package view;
+
+// import javax.swing.JButton;
+
+
+
+// public class InputButton extends JButton {
+//   String sequence;
+
+//   public InputButton(String text) {
+//     super(text);
+//     sequence = text;
+//   }
+
+
+
+
+//   public String getSequence() {
+//     return sequence;
+//   }
+// }
 package view;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class InputButton extends JButton {
-  String sequence;
+  private final String sequence;
 
   public InputButton(String text) {
     super(text);
-    sequence = text;
+    this.sequence = text;
+
+    // Styling
+    setFont(new Font("Serif", Font.BOLD, 20));
+    setFocusPainted(false);
+    setBackground(Color.WHITE);
+    setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+    setOpaque(true);
+
   }
 
   public String getSequence() {
     return sequence;
   }
 }
+
